2218da0c20459e7b914082bf92d741a1be08d3d3
Jeremy Rempel <jeremyr@interlocsolutions.com>
2016-10-29 19:26:45 -0400

    Setup user object

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,9 @@ ActiveRecord::Schema.define(version: 20161029193149) do
     t.string   "email"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.string   "password_digest"
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
