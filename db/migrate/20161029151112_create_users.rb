3727252dfe05e5fe4be0dd6d9fd374e8efb9b90f
Jeremy Rempel <jeremyr@interlocsolutions.com>
2016-10-30 11:47:00 -0400

    add user model unit tests

--- db/migrate/20161029151112_create_users.rb
+++ db/migrate/20161029151112_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :fname
-      t.string :lname
-      t.string :email
-
+      t.string :fname, limit: 50, null: false
+      t.string :lname, limit: 50, null: false
+      t.string :email, limit: 128, null: false
       t.timestamps
     end
+    
+  add_index :users, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,15 @@ ActiveRecord::Schema.define(version: 20161029193149) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "fname"
-    t.string   "lname"
-    t.string   "email"
+    t.string   "fname",              limit: 50,  null: false
+    t.string   "lname",              limit: 50,  null: false
+    t.string   "email",              limit: 128, null: false
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
