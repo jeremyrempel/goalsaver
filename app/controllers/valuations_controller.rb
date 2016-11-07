c2f2dc7f57599c79ef72787719bda3e12c82d456
Jeremy Rempel <jeremyr@interlocsolutions.com>
2016-11-06 19:23:15 -0500

    remove userid

--- app/controllers/valuations_controller.rb
+++ app/controllers/valuations_controller.rb
@@ -69,6 +69,6 @@ class ValuationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def valuation_params
-      params.require(:valuation).permit(:user_id, :asslib_id, :year, :period, :value)
+      params.require(:valuation).permit(:asslib_id, :year, :period, :value)
     end
 end
