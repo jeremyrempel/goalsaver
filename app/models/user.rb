4c8c0b9b1fdde5919cbac9c1396c36f2eeaa6fcd
Jeremy Rempel <jrempel@Jeremys-MBP.fios-router.home>
2016-11-20 14:26:32 -0500

    remove redundant hasmany asslibs/goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
     include Clearance::User
 
-    has_many :asslibs
-    has_many :goals
-
     # validations
     validates :fname, presence: true, length: { maximum: 50 }
     validates :lname, presence: true, length: { maximum: 50 }
