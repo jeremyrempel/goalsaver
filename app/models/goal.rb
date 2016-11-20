e9b2c6794c13fb9e3daebd42673828345c9b16f9
Jeremy Rempel <jrempel@Jeremys-MBP.fios-router.home>
2016-11-20 16:18:14 -0500

    add goal tests, verify value > 0

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,5 @@
 class Goal < ApplicationRecord
   validates :name, presence: true
-  validates :value, presence: true
+  validates :value, presence: true, :numericality => { :greater_than => 0 }
+
 end
--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -4,4 +4,29 @@ class GoalTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+      @goal = Goal.new(name: "One Million", value: 1000000)
+  end
+
+  # check required fields
+  test "should be valid" do
+    assert @goal.valid?
+  end
+
+  test "name should be present" do
+    @goal.name = "    "
+    assert_not @goal.valid?
+  end
+
+  test "value should be present" do
+    @goal.value = "   "
+    assert_not @goal.valid?
+  end
+
+  test "value should be positive number" do
+    @goal.value = "0"
+    assert_not @goal.valid?
+  end
+
 end
