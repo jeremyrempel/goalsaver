d7deda5e086dc9e51b47f5f9ebac05cf4a3ec681
Jeremy Rempel <jrempel@Jeremys-MBP.fios-router.home>
2016-12-18 16:09:32 -0500

    add foreign keys to valudations

--- db/migrate/20161106233312_create_valuations.rb
+++ db/migrate/20161106233312_create_valuations.rb
@@ -1,15 +1,14 @@
 class CreateValuations < ActiveRecord::Migration[5.0]
   def change
     create_table :valuations do |t|
-      t.integer :user_id
-      t.integer :asslib_id
+      t.belongs_to :user
+      t.belongs_to :asslibs
       t.integer :year
       t.string :period
       t.decimal :value
-
       t.timestamps
     end
     add_foreign_key :valuations, :users
-    add_foreign_key :valuations, :asslibs
+    #add_foreign_key :valuations, :asslibs
   end
 end
