2b4b1331e3a0d4770a7ccc902e2117fa6e3640fe
Jeremy Rempel <jrempel@Jeremys-MBP.fios-router.home>
2016-12-18 15:51:43 -0500

    update seeds, resolve some migration isssues with dup userid col

--- db/migrate/20161030182910_create_asslibs.rb
+++ db/migrate/20161030182910_create_asslibs.rb
@@ -1,14 +1,12 @@
 class CreateAsslibs < ActiveRecord::Migration[5.0]
   def change
     create_table :asslibs do |t|
-      t.belongs_to :users
+      t.belongs_to :user
       t.string :name, null: false
       t.text :description
       t.string :altype, null: false
       t.boolean :active, null: false, :default => true
       t.string :currency, null: false
-      t.integer :user_id, null: false
-
       t.timestamps
     end
     add_foreign_key :asslibs, :users
