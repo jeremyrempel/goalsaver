4c3a9b0947c576b395ca449d8179df4a216b2ada
Jeremy Rempel <jrempel@Jeremys-MBP.fios-router.home>
2017-01-16 22:27:57 -0500

    update controller model to match migration changes

--- app/models/valuation.rb
+++ app/models/valuation.rb
@@ -2,6 +2,16 @@ class Valuation < ApplicationRecord
   belongs_to :user
   belongs_to :asslib
 
+  # attr_reader
+  def period
+    self[:period]
+  end
+
+  # attr_writer
+  def period=(val)
+    self[:period] = val
+  end
+
   validates :date, presence: true
   validates :value, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
 end
