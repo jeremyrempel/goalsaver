f1ac7f4d2c7b873236d6acdc88b1cf72085feac1
Jeremy Rempel <jrempel@Jeremys-MBP.fios-router.home>
2017-02-26 21:55:29 -0500

    update test fixtures, add validations, validations for goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,11 @@
 class Goal < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :start_value, presence: true, :numericality => { :greater_than => 0 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :rate_of_return, presence: true, :numericality => { :greater_than => 0 }
+  validates :rate_of_savings, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :rate_of_savings, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :currency, presence: true, length: { maximum: 3 }
 
 end
--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -6,7 +6,8 @@ class GoalTest < ActiveSupport::TestCase
   # end
 
   def setup
-      @goal = Goal.new(name: "One Million", value: 1000000)
+      @goal = Goal.new(name: "One Million", start_value: 1000000, start_date: 2017-01-01,
+      end_date: 10.years.from_now, rate_of_return: 0.07, rate_of_savings: 1000, currency: "USD")
   end
 
   # check required fields
@@ -20,18 +21,53 @@ class GoalTest < ActiveSupport::TestCase
   end
 
   test "value should be present" do
-    @goal.value = "   "
+    @goal.start_value = "   "
     assert_not @goal.valid?
   end
 
   test "value should be positive number" do
-    @goal.value = "0"
+    @goal.start_value = 0
+    assert_not @goal.valid?
+  end
+
+  test "start date should be present" do
+    @goal.start_date = "    "
+    assert_not @goal.valid?
+  end
+
+  test "rate of return should be present" do
+    @goal.rate_of_return = "    "
+    assert_not @goal.valid?
+  end
+
+  test "rate of return should be positive number" do
+    @goal.rate_of_return = 0
+    assert_not @goal.valid?
+  end
+
+  test "rate of savings should be present and non-negative" do
+    @goal.rate_of_savings = -1
+    assert_not @goal.valid?
+  end
+
+  test "rate of savings can be zero" do
+    @goal.rate_of_savings = 0
+    assert @goal.valid?
+  end
+
+  test "currency cannot be blank" do
+    @goal.currency = "   "
     assert_not @goal.valid?
   end
 
   # length requirements
   test "goal should not be too long" do
     @goal.name = "a" * 51
+    assert_not @goal.valid?
+  end
+
+  test "currency should be 3" do
+    @goal.currency = "a" * 4
     assert_not @goal.valid?
   end
 
