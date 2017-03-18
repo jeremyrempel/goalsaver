ecca17a99e14d4a1dcafe6561e58ff7582b252bd
Jeremy Rempel <jr@idreamincolour.com>
2017-03-17 23:33:25 -0400

    Fix valuation to set the period based on date

--- app/models/valuation.rb
+++ app/models/valuation.rb
@@ -2,17 +2,30 @@ class Valuation < ApplicationRecord
   belongs_to :user
   belongs_to :asslib
 
+  attr_accessor :period
+
   # attr_writer
   def period=(val)
-    self[:period] = val
+
+    case val
+      when 'Q1'
+        self[:date] = Date.strptime('2016-01-01', '%Y-%m-%d')
+      when 'Q2'
+        self[:date] = Date.strptime('2016-04-01', '%Y-%m-%d')
+      when 'Q3'
+        self[:date] = Date.strptime('2016-07-01', '%Y-%m-%d')
+      when 'Q4'
+        self[:date] = Date.strptime('2016-10-01', '%Y-%m-%d')
+    end
+
   end
 
   def period
-    "Q" + (((Time.now.month - 1) / 3) + 1).to_s
+    'Q' + ((date.month / 3) + 1).to_s
   end
 
   validates :user, presence: true
   validates :asslib, presence: true
   validates :date, presence: true
-  validates :value, presence: true, :numericality => { :greater_than => 0 }
+  validates :value, presence: true, :numericality => {:greater_than => 0}
 end
