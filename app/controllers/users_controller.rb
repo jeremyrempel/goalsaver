57e277a3b3053b6898fa010cdfa3d259e2eb341e
Jeremy Rempel <jr@idreamincolour.com>
2017-03-18 16:17:12 -0400

    add ability to edit profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def profile
+  before_action :require_login
+  before_action :set_user, only: [:profile_edit, :profile_update]
+
+  def profile_edit; end
+
+  def profile_update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to profile_url, notice: 'Profile was successfully updated.'}
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = current_user
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:fname, :lname)
+  end
+
 end
